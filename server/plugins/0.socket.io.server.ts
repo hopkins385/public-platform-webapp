c7cffd24b4b12fe4c747b58814606fe850a7ef7b
hopkins385 <stadhouders.sven@gmail.com>
2024-05-17 11:49:56 +0200

    socket

--- server/plugins/0.socket.io.server.ts
+++ server/plugins/0.socket.io.server.ts
@@ -5,8 +5,9 @@ import { getServerSession } from '#auth';
 const logger = consola.create({}).withTag('socket-server');
 
 export default defineNitroPlugin((nitroApp: NitroApp) => {
-  const { getSocketServer } = useSocketServer();
+  const { getSocketServer, getEngine } = useSocketServer();
   const io = getSocketServer();
+  const engine = getEngine();
 
   // middleware
   io.use(async (socket, next) => {
@@ -57,4 +58,29 @@ export default defineNitroPlugin((nitroApp: NitroApp) => {
       //
     });
   });
+
+  nitroApp.router.use(
+    '/socket.io/',
+    defineEventHandler({
+      handler(event) {
+        engine.handleRequest(event.node.req, event.node.res);
+        event._handled = true;
+      },
+      websocket: {
+        open(peer) {
+          const nodeContext = peer.ctx.node;
+          const req = nodeContext.req;
+
+          // @ts-expect-error private method
+          engine.prepare(req);
+
+          const rawSocket = nodeContext.req.socket;
+          const websocket = nodeContext.ws;
+
+          // @ts-expect-error private method
+          engine.onWebSocket(req, rawSocket, websocket);
+        },
+      },
+    }),
+  );
 });
